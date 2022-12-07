915ee0102cb1286e92aa4bb0e7a674e8d579715f
DireWolf707 <rahulsood707@gmail.com>
2022-12-08 01:55:27 +0530

    added custom mui theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import { lime } from "@mui/material/colors"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const theme = createTheme({
+  palette: {
+    check: {
+      // light: "#757ce8",
+      main: lime["800"],
+      dark: lime["900"],
+      contrastText: "#fff",
+    },
+  },
+})
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )

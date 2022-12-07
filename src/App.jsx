03ae30b9db4613ce2146ad27d7fd5253b77dff45
DireWolf707 <rahulsood707@gmail.com>
2022-12-08 01:55:59 +0530

    integrated home page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import Header from "../components/Header"
+import Box from "@mui/material/Box"
+import Hero from "../components/Hero"
+
 const App = () => {
-  return ( <div>helloworld</div> );
+  return (
+    <Box sx={{ bgcolor: "dimgrey", height: "100vh", display: "flex", flexDirection: "column" }}>
+      <Header />
+      <Box sx={{ bgcolor: "dimgrey", flexGrow: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Hero />
+      </Box>
+    </Box>
+  )
 }
- 
-export default App;
+
+export default App

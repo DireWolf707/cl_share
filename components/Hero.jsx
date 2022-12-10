47d7742202b4067564cc429480b3f5cba946a659
DireWolf707 <rahulsood707@gmail.com>
2022-12-11 01:36:09 +0530

    added create/join room functionality

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,17 +1,60 @@
+import React, { useState } from "react"
+import { db, timestamp } from "../firebase/config"
+import { doc, setDoc, getDoc } from "firebase/firestore"
 import Paper from "@mui/material/Paper"
 import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
 import Typography from "@mui/material/Typography"
 import Divider from "@mui/material/Divider"
 import TextField from "@mui/material/TextField"
+import { genRandomId } from "../utils"
 
-const Hero = () => {
+const Hero = ({ setNET, setRoomId }) => {
+  const [joinRoomId, setJoinRoomId] = useState("")
+  const [joinRoomIdError, setjoinRoomIdError] = useState(false)
+  const [joinRoomIdErrorInfo, setJoinRoomIdErrorInfo] = useState("")
+
+  const createRoom = async () => {
+    try {
+      const createdAt = timestamp.fromDate(new Date())
+      const _roomId = genRandomId(6)
+      const docRef = doc(db, "rooms", _roomId)
+      await setDoc(docRef, { createdAt, files: [] })
+      setRoomId(_roomId)
+      setNET()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const joinRoom = async () => {
+    if (joinRoomId.length != 6) {
+      setjoinRoomIdError(true)
+      setJoinRoomIdErrorInfo("Room Id must be of length 6")
+      return
+    }
+    try {
+      const docRef = doc(db, "rooms", joinRoomId)
+      const room = await getDoc(docRef)
+      if (room.exists()) {
+        // console.log(room.data())
+        setRoomId(joinRoomId)
+        setNET()
+      } else {
+        setjoinRoomIdError(true)
+        setJoinRoomIdErrorInfo("Room Id does not exist")
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <Paper
       elevation={5}
       sx={{
-        height: "70%",
-        minWidth: 4 / 5,
+        height: "100%",
+        width: 1,
         border: "solid",
         display: "flex",
         justifyContent: "space-evenly",
@@ -31,23 +74,45 @@ const Hero = () => {
           </Typography>
         </Box>
       </Paper>
+
       <Divider orientation="vertical" variant="middle" flexItem />
+
       <Paper elevation={10} sx={{ bgcolor: "green", width: 3 / 7, height: "70%" }}>
         <Box sx={{ height: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
           <Typography variant="h3" component="h3" sx={{ color: "whitesmoke" }}>
             NET Connect
           </Typography>
-          <Button variant="contained" color="check" size="large" sx={{ px: 3.2, py: 1.2, mt: 3.5 }}>
+          <Button onClick={createRoom} variant="contained" color="check" size="large" sx={{ px: 3.2, py: 1.2, mt: 3.5 }}>
             Create Room
           </Button>
-          <Box sx={{ display: "flex", justifyContent: "space-evenly", alignItems: "center", width: "70%", mt: 2.2 }}>
-            <Box sx={{ bgcolor: "#D5D1D0" }}>
-              <TextField label="Room Code" variant="filled" type="text" size="small" color="error" />
-            </Box>
-            <Button variant="contained" color="check" size="large" sx={{ px: 3.2, py: 1.2 }}>
+          <Paper
+            sx={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+              width: "70%",
+              mt: 2.2,
+              bgcolor: "#E8E6E5",
+              px: 0.8,
+              py: 1,
+            }}
+          >
+            <TextField
+              value={joinRoomId}
+              onChange={(e) => setJoinRoomId(e.target.value)}
+              label="Room Code"
+              variant="filled"
+              type="text"
+              size="small"
+              color="error"
+              error={joinRoomIdError}
+              helperText={joinRoomIdErrorInfo}
+            />
+
+            <Button onClick={joinRoom} variant="contained" color="check" size="large">
               Join Room
             </Button>
-          </Box>
+          </Paper>
         </Box>
       </Paper>
     </Paper>

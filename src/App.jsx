68b8d9354988432cb6ecb7f4ee2af68a2c1cac22
DireWolf707 <rahulsood707@gmail.com>
2022-12-11 01:35:39 +0530

    added component logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import Header from "../components/Header"
 import Box from "@mui/material/Box"
 import Hero from "../components/Hero"
+import Room from "../components/Room"
+import { useState } from "react"
+
+const STATE = {
+  DASH: "dash",
+  LAN: "lan",
+  NET: "net",
+}
 
 const App = () => {
+  const [view, setView] = useState(STATE.DASH)
+  const [roomId, setRoomId] = useState(null)
+
   return (
     <Box sx={{ bgcolor: "dimgrey", height: "100vh", display: "flex", flexDirection: "column" }}>
-      <Header />
-      <Box sx={{ bgcolor: "dimgrey", flexGrow: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <Hero />
-      </Box>
+      <Header setDash={() => setView(STATE.DASH)} />
+
+      {view == STATE.DASH && (
+        <Box sx={{ flexGrow: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <Box sx={{ width: 3 / 4, height: "70%" }}>
+            <Hero setNET={() => setView(STATE.NET)} setRoomId={setRoomId} />
+          </Box>
+        </Box>
+      )}
+
+      {view == STATE.NET && roomId && (
+        <Box sx={{ display: "flex", justifyContent: "center", my: 5 }}>
+          <Box sx={{ width: 1 / 2, minHeight: "max-content" }}>
+            <Room roomId={roomId} />
+          </Box>
+        </Box>
+      )}
     </Box>
   )
 }
